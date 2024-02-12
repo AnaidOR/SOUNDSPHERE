be4f9a29222bd98035271eacdf6ebefb155ddc52
AnaidOR <89877196+AnaidOR@users.noreply.github.com>
2024-02-12 11:21:54 -0600

    Update Tabla_Anuncios.java

--- src/MODELO/ANUNCIOS/Tabla_Anuncios.java
+++ src/MODELO/ANUNCIOS/Tabla_Anuncios.java
@@ -1,5 +1,24 @@
 package MODELO.ANUNCIOS;
 
-public class Tabla_Anuncios {
+import java.util.ArrayList;
 
-}
+public class Tabla_Anuncios {
+    private ArrayList<Anuncios> anuncios;
+
+    public Tabla_Anuncios() {
+        this.anuncios = new ArrayList<>();
+    }
+
+    public void agregarAnuncios(Anuncios anuncio) {
+        anuncios.add(anuncio);
+    }
+
+    public ArrayList<Anuncios> getAnuncios() {
+        return anuncios;
+    }
+
+    public void mostrarAnuncios() {
+        for (Anuncios anuncio : anuncios) {
+            anuncio.mostrarAnuncio();
+        }
+    }
